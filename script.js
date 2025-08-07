c8308b9ea8b2a14aa7fbd95e3a78dc8f44e33d1b
DISTRICT-PUB <simonevergari88@gmail.com>
2025-08-07 08:04:35 +0200

    Update script.js

--- script.js
+++ script.js
@@ -26,7 +26,7 @@ document.addEventListener("DOMContentLoaded", () => {
   const audioWin = document.getElementById("audio-win");
 
   // Variabile di configurazione per abilitare/disabilitare il blocco settimanale
-  const weeklyBlockEnabled = true;  // Imposta a 'false' per disabilitare il blocco settimanale
+  const weeklyBlockEnabled = false;  // Imposta a 'false' per disabilitare il blocco settimanale
 
   function getPrize() {
     const r = Math.random();
@@ -138,3 +138,4 @@ document.addEventListener("DOMContentLoaded", () => {
     whatsappLink.click(); 
   });
 });
+
